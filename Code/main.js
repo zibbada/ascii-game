62244b36868458379b5c7ab9702c55b55294d59f
BuildTools <unconfigured@null.spigotmc.org>
2023-10-04 17:46:34 -0400

    player class

--- Code/main.js
+++ Code/main.js
@@ -27,22 +27,55 @@ window.addEventListener('load', function () {
                     case "KeyW": theKeymap.up = true; break;
                     case "KeyD": theKeymap.right = true; break;
                 }
-                console.log(e.code)
+            })
+            window.addEventListener('keyup', function (e) {
+                var keyPressed = e.code;
+                switch (keyPressed) {
+                    case "KeyA": theKeymap.left = false; break;
+                    case "KeyS": theKeymap.down = false; break;
+                    case "KeyW": theKeymap.up = false; break;
+                    case "KeyD": theKeymap.right = false; break;
+                }
             })
 
             
         }
     }
 
+    class Player {
+        x = screen.width / 2;
+        y = screen.height / 2;
+        
+        update() {
+            if (theKeymap.left == true) {
+                x -= 1;
+            }
+            if (theKeymap.down == true) {
+                y += 1;
+            }
+            if (theKeymap.up == true) {
+                y -= 1;
+            }
+            if (theKeymap.right == true) {
+                x += 1;
+            }
+        }
+        draw() {
+            ctx.fillRect(x-4,y-4,8,8);
+        }
+    }
+
     class Game {
         constructor() {
             this.inputHandler = new inputHandler;
+            this.player = new Player;
         }
         update() {
+            this.player.update();
         }
 
         draw() {
-
+            this.player.draw();
         }
         
     }
@@ -52,7 +85,6 @@ window.addEventListener('load', function () {
     const game = new Game();
     function animate() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-       // ctx.fillRect(x, 20, 10, 10);
         game.update();
         game.draw();
         requestAnimationFrame(animate);
