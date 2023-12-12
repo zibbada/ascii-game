a8ce98367e9bcdfa0dbbe4f79722b2ed9a46f9be
tyspe <tyspe1829@ugcloud.ca>
2023-12-12 15:28:56 -0500

    Collisions and possibly 3d

--- Code/Map.js
+++ Code/Map.js
@@ -5,11 +5,42 @@ class Map {
         this.tileSet = new Image(0, 0)
         this.zoom = 5;
         this.tileSet.src = "./tileset.png";
+
     }
     Update() {
 
     }
     Draw(ctx, camX, camY) {
+        let alpha = 1 / 2;
+        for (let z = 0; z < 1; z++) {
+            let mapindex = 0;
+            for (let y = 0; y < this.currentMap.size.height; y++) {
+                for (let x = 0; x < this.currentMap.size.width; x++) {
+
+                    let dx = ((x)) - (x) * 1.1;
+                    //console.log(dx) , (x - camX + dx*z) * this.tileSize * this.zoom * (1 + 1 * 0.1);
+                    let tileVal = this.currentMap.map[mapindex] - 1;
+                    let sx = (tileVal % 27) * this.tileSize;
+                    let sy = Math.floor(tileVal / 27) * this.tileSize;
+                    ctx.globalAlpha = alpha * (z + 1);
+                    ctx.drawImage(this.tileSet,
+                        sx,
+                        sy,
+                        this.tileSize,
+                        this.tileSize,
+                        (x - camX + dx*z) * this.tileSize * this.zoom * (1 + z * 0.1),
+                        (y - camY) * this.tileSize * this.zoom * (1 + z * 0.1),
+                        this.tileSize * (this.zoom),
+                        this.tileSize * (this.zoom));
+                    mapindex++;
+                }
+            }
+        }
+    }
+}
+
+/*
+  Draw(ctx, camX, camY) {
         let alpha = 1 / 2;
         for (let z = 0; z < 2; z++) {
             let mapindex = 0;
@@ -24,13 +55,25 @@ class Map {
                         sy,
                         this.tileSize,
                         this.tileSize,
-                        (x - camX) * this.tileSize * this.zoom,
-                        (y - z- camY) * this.tileSize * this.zoom * (1 + z * 0.05),
+                        (x - 2.5*z - camX) * this.tileSize * this.zoom * (1 + z * 0.1),
+                        (y - 1.40*z - camY) * this.tileSize * this.zoom * (1 + z * 0.1),
                         this.tileSize * (this.zoom),
                         this.tileSize * (this.zoom));
+                        if (z>0){
+                            ctx.drawImage(this.tileSet,
+                                sx,
+                                sy,
+                                this.tileSize,
+                                this.tileSize,
+                                (x - 1.30*z - camX) * this.tileSize * this.zoom * (1 + z * 0.05),
+                                (y - 0.75*z - camY) * this.tileSize * this.zoom * (1 + z * 0.05),
+                                this.tileSize * (this.zoom),
+                                this.tileSize * (this.zoom));  
+                        }
                     mapindex++;
                 }
             }
         }
     }
-}
+    */
+   
--- Code/Player.js
+++ Code/Player.js
@@ -1,24 +1,23 @@
 class Player {
    constructor () {
        this.x = 12;
-       this.y = 12;
-       console.log(this.x + " " + this.y)
+       this.y = 12 ;
    }
     
     update(theKeymap) {
-        if (theKeymap.left == true) {
+        if (theKeymap.left == true && lvl1.collision[this.y][this.x-1] != 1) {
             this.x -= 1;
-            theKeymap.left = false;
+            theKeymap.left = false ;
         }
-        if (theKeymap.down == true) {
+        if (theKeymap.down == true && lvl1.collision[this.y+1][this.x] != 1) {
             this.y += 1;
             theKeymap.down = false;
         }
-        if (theKeymap.up == true) {
+        if (theKeymap.up == true && lvl1.collision[this.y-1][this.x] != 1) {
             this.y -= 1;
            theKeymap.up = false;
         }
-        if (theKeymap.right == true) {
+        if (theKeymap.right == true && lvl1.collision[this.y][this.x+1] != 1) {
             this.x += 1;
             theKeymap.right = false;
         }
